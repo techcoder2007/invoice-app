ffff502d81045b3a94af182a4da37b72513d9001
techcoder2007 <techcoder444@gmail.com>
2024-11-16 23:50:01 +0500

    added label input and textarea components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -6,11 +8,33 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import Link from "next/link";
 
 export default function DashboardPage() {
   return (
-    <main>
-      <h1 className="text-4xl font-bold">Dashboard Page</h1>
+    <main className="container max-w-5xl mx-auto flex flex-col items-center justify-center h-screen">
+      <div className="flex items-center justify-between w-full mb-2">
+        <h1 className="text-4xl font-bold">Invoices</h1>
+        <Button asChild variant="outline">
+          <Link href="/invoices/new">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            Create Invoice
+          </Link>
+        </Button>
+      </div>
       <Table>
         <TableHeader>
           <TableRow>
@@ -23,10 +47,13 @@ export default function DashboardPage() {
         </TableHeader>
         <TableBody>
           <TableRow>
-            <TableCell>INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell>$250.00</TableCell>
+            <TableCell>01/01/2023</TableCell>
+            <TableCell>John Doe</TableCell>
+            <TableCell>gBk4R@example.com</TableCell>
+            <TableCell>
+              <Badge variant="default">Open</Badge>
+            </TableCell>
+            <TableCell>$100</TableCell>
           </TableRow>
         </TableBody>
       </Table>
---
+++ src/app/invoices/new/page.tsx
@@ -0,0 +1,9 @@
+export default function CreateInvoice() {
+  return (
+    <main className="container max-w-5xl mx-auto flex flex-col items-center justify-center h-screen">
+      <div className="flex items-center justify-between w-full mb-2">
+        <h1 className="text-4xl font-bold">Create new invoice </h1>
+      </div>
+    </main>
+  );
+}
