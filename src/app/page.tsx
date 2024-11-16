491575d3cf0804aa865988772ab3a6579fcc17ce
techcoder2007 <techcoder444@gmail.com>
2024-11-16 23:34:36 +0500

    added table component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>home page</h1>
-    </div>
+    <main className="max-w-5xl mx-auto flex flex-col items-center justify-center h-screen text-center">
+      <h1 className="text-4xl font-bold">Invoicipedia</h1>
+      <Button asChild>
+        <a href="/sign-in">Sign In</a>
+      </Button>
+    </main>
   );
 }
